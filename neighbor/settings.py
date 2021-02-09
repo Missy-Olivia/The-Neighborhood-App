de915b958e7f096387a34536892b875e94a82534
Ngabire Olivia Missy <ngamissia@gmail.com>
2021-02-09 11:06:20 +0200

    edits

--- neighbor/settings.py
+++ neighbor/settings.py
@@ -11,9 +11,14 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-
+import os
+import django_heroku
+import dj_database_url
+from decouple import config,Csv
+LOGIN_REDIRECT_URL = '/login/'
+LOGIN_URL = '/login/'
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
